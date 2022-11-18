2d51c9e88838c532c5af7c74546a32da7c49922f
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-11-18 12:12:50 +0600

    Added header city widget :dizzy:

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:weathersec/controller/global_controller.dart';
+import 'package:weathersec/widgets/header_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,6 +18,21 @@ class _HomeScreenState extends State<HomeScreen> {
       Get.put(GlobalController(), permanent: true);
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Obx(
+          () => globalController.checkLoading().isTrue
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView(
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    const HeaderWidget(),
+                  ],
+                ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/header_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:weathersec/controller/global_controller.dart';
+
+class HeaderWidget extends StatefulWidget {
+  const HeaderWidget({super.key});
+
+  @override
+  State<HeaderWidget> createState() => _HeaderWidgetState();
+}
+
+class _HeaderWidgetState extends State<HeaderWidget> {
+  String city = "";
+  final GlobalController globalController =
+      Get.put(GlobalController(), permanent: true);
+
+  @override
+  void initState() {
+    getAddress(globalController.getLattitude().value,
+        globalController.getLongitude().value);
+    super.initState();
+  }
+
+  getAddress(lat, lon) async {
+    List<Placemark> placemark = await placemarkFromCoordinates(lat, lon);
+    Placemark place = placemark[0];
+    setState(() {
+      city = place.locality!;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          child: Text(city ),
+        ),
+      ],
+    );
+  }
+}
